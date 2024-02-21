d3d05f0b9f7087a83ec18f83667de9036d091cf4
reva3yasmin7 <160677120+reva3yasmin7@users.noreply.github.com>
2024-02-21 09:09:46 +0700

    Update MTitik.java

--- MTitik.java
+++ MTitik.java
@@ -1,1 +1,13 @@
-
+class MTitik
+{
+	public static void main(String[] args)
+	{
+		Titik t1, t2;
+		t1 = new Titik(); t1.setAbsis(1);t1.setOrdinat(2);
+		t2 = new Titik(); t2.setAbsis(3);t1.setOrdinat(4);
+		System.out.println("Jumlah objek titik : " + Titik.getCounterTitik());
+		System.out.println("t1" +"(" + t1.getAbsis() + "," + t1.getOrdinat() + ")");
+		System.out.println("t2" +"(" + t2.getAbsis() + "," + t2.getOrdinat() + ")");
+		
+	}
+}
