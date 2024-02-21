a34c14fe2c3ecfe51cbb71f696ecc2a77bb3d3bc
reva3yasmin7 <160677120+reva3yasmin7@users.noreply.github.com>
2024-02-21 09:29:53 +0700

    Update MTitik.java
    menambahkan deskripsi program dan pembuat program, mengedit nama variabel program

--- MTitik.java
+++ MTitik.java
@@ -1,13 +1,20 @@
+/**
+ * File      : MTitik.java		21-02-2024
+ * Nama penulis   : Reva Yasmin Naufalia
+ * NIM               : 24060122130075
+ * Deskripsi : Kelas yang berisi main  dari Titik
+ * 
+ */
 class MTitik
 {
 	public static void main(String[] args)
 	{
-		Titik t1, t2;
-		t1 = new Titik(); t1.setAbsis(1);t1.setOrdinat(2);
-		t2 = new Titik(); t2.setAbsis(3);t1.setOrdinat(4);
+		Titik p1, p2;
+		p1 = new Titik(); p1.setAbsis(1);p1.setOrdinat(2);
+		p2 = new Titik(); p2.setAbsis(3);p1.setOrdinat(4);
 		System.out.println("Jumlah objek titik : " + Titik.getCounterTitik());
-		System.out.println("t1" +"(" + t1.getAbsis() + "," + t1.getOrdinat() + ")");
-		System.out.println("t2" +"(" + t2.getAbsis() + "," + t2.getOrdinat() + ")");
+		System.out.println("t1" +"(" + p1.getAbsis() + "," + p1.getOrdinat() + ")");
+		System.out.println("t2" +"(" + p2.getAbsis() + "," + p2.getOrdinat() + ")");
 		
 	}
 }
